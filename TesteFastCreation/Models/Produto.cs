69282b8ac5b65443c87accc0d804fa16de000569
Sousa <railson.rames.sousa@accenture.com>
2020-02-04 12:08:46 -0300

    Add project files.

---
+++ TesteFastCreation/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteFastCreation.Models
+{
+    public class Produto
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Codigo { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
